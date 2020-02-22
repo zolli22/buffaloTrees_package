d5fe1089774da3b26531fb9c4d90e2ccf50986fd
David Herrero <daherrero@reed.edu>
2020-02-22 10:09:45 -0800

    Documentation, description, and readme done.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,7 +7,9 @@ tree_inventory <- read_csv("data-raw/Tree_Inventory.csv")
 
 # Clean up names
 buffaloTrees <- tree_inventory %>%
-  clean_names()
+  clean_names() %>%
+  rename(kwh_saved = k_wh_saved,
+         geocoded_column = location)
 
 # Add datafiles to the project
 usethis::use_data(buffaloTrees, overwrite = TRUE)
